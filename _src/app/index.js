8274fba83da297393172f9f7d63220cbd8dd8d11
Matt Smith <msmith@apaxsoftware.com>
2016-10-07 08:23:49 -0400

    Move  to factory service.

--- _src/app/index.js
+++ _src/app/index.js
@@ -1,21 +1,32 @@
 import angular from 'angular';
 
-function AppController($http) {
+function randomuserService ($http) {
+    function getUsers (seed) {
+        return $http.get('https://randomuser.me/api/', { 
+            params: {
+                results: 25,
+                seed: seed,
+            }
+        });
+    }
+
+    return {
+        getUsers: getUsers,
+    }
+}
+
+function AppController(randomuserService) {
     const ctrl = this;
     ctrl.seed = null;
     ctrl.users = [];
     ctrl.currentUser = null;
 
     function updateUsers () {
-        $http.get('https://randomuser.me/api/', { 
-            params: {
-                results: 25,
-                seed: ctrl.seed,
-            }
-        }).then(function (response) {
-            ctrl.users = response.data.results;
-            ctrl.currentUser = null;
-        });
+        randomuserService.getUsers(ctrl.seed)
+            .then(function (response) {
+                ctrl.users = response.data.results;
+                ctrl.currentUser = null;
+            });
     }
 
     updateUsers();
@@ -33,6 +44,7 @@ function AppController($http) {
 const AppModule = angular.module('app', [
 ]);
 
+AppModule.factory('randomuserService', randomuserService);
 AppModule.controller('AppController', AppController);
 
 export default AppModule;
