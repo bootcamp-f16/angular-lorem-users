3643951136cf09917ca886c95db112dae8685270
Matt Smith <msmith@apaxsoftware.com>
2016-10-07 08:46:26 -0400

    Move component and controller to their own file.

--- _src/app/index.js
+++ _src/app/index.js
@@ -3,43 +3,13 @@ import 'angular-resource';
 
 import randomuserService from './randomuser.service';
 import appTemplate from './app.html';
-
-
-function AppController(randomuserService) {
-    const ctrl = this;
-    ctrl.seed = null;
-    ctrl.users = [];
-    ctrl.currentUser = null;
-
-    function updateUsers () {
-        randomuserService.getUsers({seed: ctrl.seed}).$promise
-            .then(function (users) {
-                ctrl.users = users;
-                ctrl.currentUser = null;
-            });
-    }
-
-    updateUsers();
-
-    ctrl.update = function () {
-        ctrl.seed = ctrl.newSeed;
-        updateUsers();
-    }
-
-    ctrl.setUserDetail = function (index) {
-        ctrl.currentUser = ctrl.users[index];
-    }
-}
+import appComponent from './app.component';
 
 const AppModule = angular.module('app', [
     'ngResource'
 ]);
 
 AppModule.factory('randomuserService', randomuserService);
-AppModule.component('app', {
-    template: appTemplate,
-    controller: AppController,
-    controllerAs: 'appCtrl'
-});
+AppModule.component('app', appComponent);
 
 export default AppModule;
